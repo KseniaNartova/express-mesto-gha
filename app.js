51f8f48ddbdc2868fbc1fd399ab56345e1aafc94
Nartova Ksenia <xenianartova@yandex.ru>
2022-12-12 16:33:53 +0800

    1 rev

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const cards = require('./routes/card');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-});
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -19,10 +16,14 @@ app.use((req, res, next) => {
   next();
 });
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
 app.use('/users', users);
 app.use('/cards', cards);
 
-app.all('*', (req, res) => {
+app.all('/*', (req, res) => {
   res.status(NOT_FOUND).send({ message: 'Cтраницa не существует' });
 });
 
--- controller/card.js
+++ controller/card.js
@@ -35,8 +35,7 @@
 //     .then((card) => res.status(CREATED).send(card))
 //     .catch((err) => {
 //       if (err.name === 'ValidationError') {
-//         res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные
-' });
+//         res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
 //         return;
 //       }
 //       res.status(SERVER_ERROR).send({ message: 'Сервер столкнулся с неожиданной ошибкой, которая помешала ему выполнить запрос' });
@@ -110,13 +109,10 @@
 
 const Card = require('../models/card');
 const {
-  BAD_REQUEST,
-  NOT_FOUND,
-  SERVER_ERROR,
-  CREATED,
+  BAD_REQUEST, NOT_FOUND, SERVER_ERROR, CREATED,
 } = require('../utils/constants');
 
-module.exports.getCards = (req, res) => {
+module.exports.getCard = (req, res) => {
   Card.find({})
     .populate(['owner', 'likes'])
     .then((cards) => res.send({ data: cards }))
