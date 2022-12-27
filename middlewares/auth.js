50e8a88b6522def54a9ab7239e51cfa05eb6e1a8
Nartova Ksenia <xenianartova@yandex.ru>
2022-12-28 00:46:59 +0800

    1 rev

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,55 +1,55 @@
-// const jwt = require('jsonwebtoken');
-// const AuthError = require('../errors/AuthError');
-
-// const { SECRET_JWT = 'very-secret-key' } = process.env;
-
-// // eslint-disable-next-line consistent-return
-// module.exports = (req, res, next) => {
-//   const { authorization } = req.headers;
-//   if (!authorization || !authorization.startsWith('Bearer ')) {
-//     return Promise.reject(new AuthError('Необходима авторизация'));
-//   }
-//   const token = authorization.replace('Bearer ', '');
-//   let payload;
-//   try {
-//     payload = jwt.verify(token, SECRET_JWT);
-//   } catch (err) {
-//     return Promise.reject(new AuthError('Необходима авторизация'));
-//   }
-//   req.user = payload;
-
-//   next();
-// };
-
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/AuthError');
 
 const { SECRET_JWT = 'very-secret-key' } = process.env;
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(
-      new AuthError({
-        message: 'Необходима авторизация',
-      }),
-    );
+    return next(new AuthError('Необходима авторизация'));  
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, SECRET_JWT);
   } catch (err) {
-    if (err.name === 'JsonWebTokenError') {
-      return next(
-        new AuthError({
-          message: 'Необходима авторизация',
-        }),
-      );
-    }
-    return next(err);
+    return next(new AuthError('Необходима авторизация'));  
   }
   req.user = payload;
+
   next();
-  return payload;
 };
+
+// const jwt = require('jsonwebtoken');
+// const AuthError = require('../errors/AuthError');
+
+// const { SECRET_JWT = 'very-secret-key' } = process.env;
+
+// module.exports = (req, res, next) => {
+//   const { authorization } = req.headers;
+//   if (!authorization || !authorization.startsWith('Bearer ')) {
+//     return next(
+//       new AuthError({
+//         message: 'Необходима авторизация',
+//       }),
+//     );
+//   }
+//   const token = authorization.replace('Bearer ', '');
+//   let payload;
+//   try {
+//     payload = jwt.verify(token, SECRET_JWT);
+//   } catch (err) {
+//     if (err.name === 'JsonWebTokenError') {
+//       return next(
+//         new AuthError({
+//           message: 'Необходима авторизация',
+//         }),
+//       );
+//     }
+//     return next(err);
+//   }
+//   req.user = payload;
+//   next();
+//   return payload;
+// };
