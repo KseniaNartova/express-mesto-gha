13fb9bdbab52f86ae577c4f20552e3c2023f83b8
Nartova Ksenia <xenianartova@yandex.ru>
2022-12-28 13:21:35 +0800

    1 rev

--- controller/card.js
+++ controller/card.js
@@ -27,11 +27,20 @@ module.exports.deleteCard = (req, res, next) => {
   const { cardId } = req.params;
   Card.findByIdAndRemove(cardId)
     .orFail(new NotFoundError('Карточка не найдена'))
+    // .then((card) => {
+    //   if (card.owner._id.toString() !== req.user._id) {
+    //     next(new ForbiddenError('Вы не являетесь автором карточки'));
+    //   } else {
+    //     throw new ForbiddenErr('Вы не можете удалить чужую карточку');
+    //   }
+    //   return Card.findByIdAndRemove(req.params.cardId).then(() => res.send(card));
+    // })
     .then((card) => {
-      if (card.owner._id.toString() !== req.user._id) {
-        next(new ForbiddenError('Вы не являетесь автором карточки'));
+      if (card.owner._id.toString() === req.user._id) {
+        res.send({ removed: true, data: card });
+      } else {
+        throw new ForbiddenError('Вы не являетесь автором карточки');
       }
-      return Card.findByIdAndRemove(req.params.cardId).then(() => res.send(card));
     })
     .catch((err) => {
       if (err.name === 'CastError') {
