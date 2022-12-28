d4478e93acb09b339412320d38de9ef94d77dae1
Nartova Ksenia <xenianartova@yandex.ru>
2022-12-28 10:38:37 +0800

    1 rev

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const { handlerError } = require('./middlewares/handlerError');
+const handlerError = require('./middlewares/handlerError');
 const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
